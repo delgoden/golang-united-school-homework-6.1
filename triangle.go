c5d635d0ad99e4ceaaba56913a066a4d47b578c0
Denis Ravinski <delgoden@gmail.com>
2022-05-28 11:52:15 +0300

    implements methods for calculations area and perimeter for triangle

--- triangle.go
+++ triangle.go
@@ -1,6 +1,16 @@
 package golang_united_school_homework
 
+import "math"
+
 // Triangle must satisfy to Shape interface
 type Triangle struct {
 	Side float64
+}
+
+func (t Triangle) CalcPerimeter() float64 {
+	return 3.0 * t.Side
+}
+
+func (t Triangle) CalcArea() float64 {
+	return t.Side * t.Side * math.Sqrt(3.0) / 4.0
 }
